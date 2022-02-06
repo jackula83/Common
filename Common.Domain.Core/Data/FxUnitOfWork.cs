55b039e378684a90972d1ccefb9bcc9478b5acf1
jackula83 <jackula@gmail.com>
2022-02-06 22:35:38 +1100

    add common unit of work

---
+++ Common.Domain.Core/Data/FxUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain.Core.Data
+{
+    public abstract class FxUnitOfWork<TContext> : IDisposable
+        where TContext : FxDbContext
+    {
+        private readonly TContext _context;
+        private object _scope = new object();
+        private bool _disposed = false;
+
+        public FxUnitOfWork(TContext context)
+            => _context = context;
+
+        public virtual void Save()
+            => _context.SaveChanges();
+
+        public virtual void Dispose()
+        {
+            lock(_scope)
+            {
+                if (!_disposed)
+                    _context.Dispose();
+                _disposed = true;
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
+}
