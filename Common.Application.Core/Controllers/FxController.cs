65dccf529ec34cf2e1bc4cd2383e936a6b2c22cc
jackula83 <jackula@gmail.com>
2022-04-22 21:45:31 +1000

    checkpoint for entity controllers

--- Common.Application.Core/Controllers/FxController.cs
+++ Common.Application.Core/Controllers/FxController.cs
@@ -1,19 +1,45 @@
 ﻿using Common.Application.Core.Models;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mime;
 
 namespace Common.Application.Core.Controllers
 {
-    public abstract class FxController<TControllerRequest, TQueryRequest, TCommandRequest> : ControllerBase
-           where TControllerRequest : FxControllerRequest
-           where TQueryRequest : FxQueryRequest, new()
-           where TCommandRequest : FxCommandRequest, new()
+    public abstract class FxController : ControllerBase
     {
         protected readonly IMediator _mediator;
+        protected readonly ILogger<FxController> _logger;
 
-        protected FxController(IMediator mediator)
+        protected FxController(IMediator mediator, ILogger<FxController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet("health/")]
+        public IActionResult Health()
+            => Ok(this.GetType().Name + " is working!");
+
+        protected ContentResult JsonContent(object content)
+            => Content(JsonConvert.SerializeObject(content), MediaTypeNames.Application.Json);
+
+        protected async Task<IActionResult> Send<TRequest>(TRequest request)
+            where TRequest : FxMediatorRequest, new()
+        {
+            try
+            {
+                var response = await _mediator.Send(request);
+                return this.JsonContent(response!);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, default);
+            }
+
+            return StatusCode((int) HttpStatusCode.InternalServerError);
         }
     }
 }
