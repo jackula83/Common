d619119cc65f5d9ca81e4df9da26870bdae0850b
jackula83 <jackula@gmail.com>
2023-02-12 14:17:40 +1100

    added DaprQueue for publishing to the MQ configured via Dapr

---
+++ src/Framework2.Infra.MQ/DaprMQ/DaprConfig.cs
@@ -0,0 +1,12 @@
+﻿
+using Framework2.Infra.MQ.Core;
+
+namespace Framework2.Infra.MQ.DaprMQ
+{
+    public sealed class DaprConfig : FxEnv
+    {
+        private static string Prefix => "Dapr_Mq";
+
+        public static string QueueName => BuildEnvName(Prefix, nameof(QueueName));
+    }
+}
---
+++ src/Framework2.Infra.MQ/DaprMQ/DaprQueue.cs
@@ -0,0 +1,20 @@
+﻿using Dapr.Client;
+using Framework2.Infra.MQ.Core;
+
+namespace Framework2.Infra.MQ.DaprMQ
+{
+    public class DaprQueue : IPublishQueue
+    {
+        private readonly DaprClient _client;
+
+        public DaprQueue()
+        {
+            _client = new DaprClientBuilder().Build();
+        }
+
+        public async Task Publish<TEvent>(TEvent @event) where TEvent : FxEvent
+        {
+            await _client.PublishEventAsync(DaprConfig.QueueName, @event.Name, @event);
+        }
+    }
+}
